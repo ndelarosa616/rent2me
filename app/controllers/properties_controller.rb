76774caea647baf803331c5df21da9131aec4bd3
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-07-16 22:14:30 -0400

    added edit update show forms views etc

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -8,8 +8,36 @@ class PropertiesController < ApplicationController
   end
 
   def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      flash[:notice] = "Property was saved."
+      redirect_to @property
+    else
+      flash[:error] = "There was an error saving the property. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @property = Property.find(params[:id])
   end
+
+  def update
+    @property = Property.find(params[:id])
+    if @property.update_attributes(property_params)
+      flash[:notice] = "Property was updated."
+      redirect_to @property
+    else
+      flash[:error] = "There was an error saving the property. Please try again."
+      render :edit
+    end
+  end
+end
+
+def property_params
+  params.require(:property).permit(:name, :address1, :address2, :address3, :country, :city, :state, :zip, :taxes, :rent, :notes)
 end
